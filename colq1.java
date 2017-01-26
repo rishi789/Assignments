e43d63dda68829202d28e29f82e4537f1671b674
Anupam Pandey <sunnysuccess22@gmail.com>
2017-01-26 15:38:42 +0530

    Add files via upload
    first program of collection assignments.
    You share your own code or modify my code for removing errors by cloning this repository

---
+++ colq1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class duplicate{
+public Static void main(String args[]){
+Iterator itr=dup.getKey().Iterator()
+public int repeated;
+while(itr.hasNext())
+{
+names n=(names)itr.next();
+int key=n.hashcode();
+while(itr.hasNext()){
+if(key.equals(itr.next())){
+cnt++;
+}}
+if(cnt>1)
+repeated=repeated+1;
+}
+system.out.println("the number of names repeated are"+repeated);
+Set<names> dup=new HashSet<names>()
+dup.add(new names(1,"raudra"));
+dup.add(new names(2,"ravi"));
+dup.add(new names(3,"java"));
+dup.add(new names(4,"ravula"));
+dup.add(new names(2,"ravi"));
+dup.add(new names(4,"ravula")); //override hashcode and equals method
+System.out.println(dup);
+}
+}
+public class names
+{
+public int key;
+public string name;
+names(ïnt key,string name)
+{
+this.key=key;
+this.name=name;
+}
+public int hashcode(){
+return key;
+}
+public boolean equals(Object o)
+{
+if(!(o instance of names))
+return false;
+else
+return this.key==(names)o.key;
+}
+
